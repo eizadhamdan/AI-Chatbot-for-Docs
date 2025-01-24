1d57bb6e11ce9c80a54f3d4d12e14659c287dc24
eizadhamdan <115660876+eizadhamdan@users.noreply.github.com>
2025-01-24 23:29:05 +0530

    update embedding

--- AI_for_PDFs/embedding_function.py
+++ AI_for_PDFs/embedding_function.py
@@ -1,10 +1,24 @@
 from langchain_community.embeddings.ollama import OllamaEmbeddings
 from langchain_aws import BedrockEmbeddings
+from langchain_openai import OpenAIEmbeddings
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 
 def get_embedding_function():
     # embeddings = BedrockEmbeddings(
     #     credentials_profile_name="default", region_name="us-east-1"
     # )
-    embeddings = OllamaEmbeddings(model="nomic-embed-text")
+    
+    # embeddings = OllamaEmbeddings(model="nomic-embed-text")
+
+    api_key = os.getenv("OPENAI_API_KEY")
+    embeddings = OpenAIEmbeddings(
+        model="text-embedding-ada-002",
+        openai_api_key=api_key
+    )
+
     return embeddings
